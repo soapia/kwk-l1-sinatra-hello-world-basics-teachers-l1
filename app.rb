b3556003df18e13b6964d99b181028768186770c
sofia ongele <sofiaongele@gmail.com>
2018-06-12 02:01:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
-class App < Sinatra::Base
+class App < Sinatra::Base # your app is inheriting from the prexisting class that makes sinatra work
 
   # Define your GET '/' route below and respond with "Hello, World!"
-  get '/' do
-    "Hello, World!"
+  get '/' do # this is saying what you want to do at a certain link
+    "Hello, World!" # tadaaaaaa should show in the terminal
   end
 end
