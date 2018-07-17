8369ca4373dc574145d405d1e095276dcbc11c44
sofia ongele <sofiaongele@gmail.com>
2018-07-17 16:57:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,8 @@ class App < Sinatra::Base # your app is inheriting from the prexisting class tha
   get '/' do # this is saying what you want to do at a certain link
     "Hello, World!" # tadaaaaaa should show in the terminal
   end
+  
+  get '/hi' do
+    "hi"
+  end
 end
